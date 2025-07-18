2fd18f98a76e66884c5c610c2c0a2d01626a5db7
fakhri2406 <Gezal_yv57@itstep.edu.az>
2025-07-18 16:27:08 +0400

    feat: enable AspectJ auto proxy support in the application

--- src/main/java/com/creadev/CreadevApplication.java
+++ src/main/java/com/creadev/CreadevApplication.java
@@ -2,8 +2,10 @@ package com.creadev;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @SpringBootApplication
+@EnableAspectJAutoProxy
 public class CreadevApplication {
 
     public static void main(String[] args) {
