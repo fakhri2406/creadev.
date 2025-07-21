9c6153780887593faad60123e4d5652138982699
fakhri2406 <Gezal_yv57@itstep.edu.az>
2025-07-21 21:31:27 +0400

    feat: update database to Azure SQL Database

--- src/main/resources/db.migration/V1__init_schema.sql
+++ src/main/resources/db.migration/V1__init_schema.sql
@@ -1,10 +1,10 @@
 CREATE TABLE roles (
-    id SERIAL PRIMARY KEY,
+    id INT IDENTITY(1,1) PRIMARY KEY,
     title VARCHAR(255) NOT NULL UNIQUE
 );
 
 CREATE TABLE users (
-    id SERIAL PRIMARY KEY,
+    id INT IDENTITY(1,1) PRIMARY KEY,
     username VARCHAR(255) NOT NULL UNIQUE,
     password_hash VARCHAR(255) NOT NULL,
     password_salt VARCHAR(255),
@@ -12,14 +12,14 @@ CREATE TABLE users (
     last_name VARCHAR(255) NOT NULL,
     email VARCHAR(255) NOT NULL UNIQUE,
     phone_number VARCHAR(255) NOT NULL UNIQUE,
-    registered_at TIMESTAMP NOT NULL,
-    last_login_at TIMESTAMP,
+    registered_at DATETIME2 NOT NULL,
+    last_login_at DATETIME2,
     role_id INTEGER NOT NULL,
     CONSTRAINT fk_users_role FOREIGN KEY (role_id) REFERENCES roles(id)
 );
 
 CREATE TABLE user_roles (
-    id SERIAL PRIMARY KEY,
+    id INT IDENTITY(1,1) PRIMARY KEY,
     user_id INTEGER NOT NULL,
     role_id INTEGER NOT NULL,
     CONSTRAINT fk_user_roles_user FOREIGN KEY (user_id) REFERENCES users(id),
@@ -27,15 +27,15 @@ CREATE TABLE user_roles (
 );
 
 CREATE TABLE refresh_tokens (
-    id SERIAL PRIMARY KEY,
+    id INT IDENTITY(1,1) PRIMARY KEY,
     token VARCHAR(255) NOT NULL UNIQUE,
-    expires_at TIMESTAMP NOT NULL,
+    expires_at DATETIME2 NOT NULL,
     user_id INTEGER NOT NULL,
     CONSTRAINT fk_refresh_tokens_user FOREIGN KEY (user_id) REFERENCES users(id)
 );
 
 CREATE TABLE revoked_tokens (
-    id SERIAL PRIMARY KEY,
+    id INT IDENTITY(1,1) PRIMARY KEY,
     token VARCHAR(255) NOT NULL UNIQUE,
     expires_at TIMESTAMP NOT NULL
 );
--- src/main/resources/db.migration/V2__create_categories_and_products_tables.sql
+++ src/main/resources/db.migration/V2__create_categories_and_products_tables.sql
@@ -1,18 +1,18 @@
 CREATE TABLE categories (
-    id SERIAL PRIMARY KEY,
+    id INT IDENTITY(1,1) PRIMARY KEY,
     title VARCHAR(100) NOT NULL,
     description VARCHAR(200),
-    created_at TIMESTAMP NOT NULL
+    created_at DATETIME2 NOT NULL
 );
 
 CREATE TABLE products (
-    id SERIAL PRIMARY KEY,
+    id INT IDENTITY(1,1) PRIMARY KEY,
     category_id INTEGER NOT NULL,
     title VARCHAR(100) NOT NULL,
     description VARCHAR(1000),
     link VARCHAR(255),
     image_url VARCHAR(255) NOT NULL,
-    created_at TIMESTAMP NOT NULL,
-    updated_at TIMESTAMP NOT NULL,
+    created_at DATETIME2 NOT NULL,
+    updated_at DATETIME2 NOT NULL,
     CONSTRAINT fk_products_category FOREIGN KEY (category_id) REFERENCES categories(id)
 ); 
