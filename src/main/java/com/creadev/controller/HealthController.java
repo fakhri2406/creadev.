be1a06cfc235065aaff017f74c30f0960b850933
fakhri2406 <Gezal_yv57@itstep.edu.az>
2025-07-21 23:31:54 +0400

    feat: add HealthController with /ping endpoint for health check

---
+++ src/main/java/com/creadev/controller/HealthController.java
@@ -0,0 +1,13 @@
+package com.creadev.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthController {
+    @GetMapping("/ping")
+    public ResponseEntity<String> ping() {
+        return ResponseEntity.ok("pong");
+    }
+} 
